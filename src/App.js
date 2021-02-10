4b3ef8b834750ddcac1477bf6b2b9c31b5213563
xelio09 <magnus60128@gmail.com>
2021-02-10 10:24:14 +0100

    adds clock

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Bikes from "./Bikes"
+import Clock from "./Clock";
 import DadJokes from "./DadJokes";
 
 
 function App() {
   return (
     <div className="App">
+        <Clock />
         <DadJokes />
         <Bikes />
     </div>
